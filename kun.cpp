8c7b80ca13af91b1cd3e64c3b19a8ebc25472ca4
Dheka04 <fickyalifian@gmail.com>
2025-06-05 15:04:41 +0700

    tic tac toe ai

--- kun.cpp
+++ kun.cpp
@@ -1,127 +1,136 @@
-//Tic Tac Toe Game in C++
-
-//Importing the inbuild libraries in CPP
 #include <iostream>
-#include <stdlib.h>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
-//Array for the board
-char board[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
-//Variable Declaration
-int choice;
-int row,column;
-char turn = 'X';
-bool draw = false;
 
-//Function to show the current status of the gaming board
+class TicTacToe {
+private:
+    char board[3][3];
+    char turn;
+    stack<int> moveHistory;
+    queue<string> giliran;
 
-void display_board(){
-
-    //Rander Game Board LAYOUT
-
-    cout<<"PLAYER - 1 [X]t PLAYER - 2 [O]nn";
-    cout << endl;
-    cout<<"tt     |     |     n";
-    cout<<"tt  "<<board[0][0]<<"  | "<<board[0][1]<<"  |  "<<board[0][2]<<" n";
-    cout<<"tt_____|_____|_____n";
-    cout<<"tt     |     |     n";
-    cout<<"tt  "<<board[1][0]<<"  | "<<board[1][1]<<"  |  "<<board[1][2]<<" n";
-    cout<<"tt_____|_____|_____n";
-    cout<<"tt     |     |     n";
-    cout<<"tt  "<<board[2][0]<<"  | "<<board[2][1]<<"  |  "<<board[2][2]<<" n";
-    cout<<"tt     |     |     n";
-    cout << endl;
-}
-
-//Function to get the player input and update the board
-
-void player_turn(){
-    if(turn == 'X'){
-        cout<<"ntPlayer - 1 [X] turn : ";
-    }
-    else if(turn == 'O'){
-        cout<<"ntPlayer - 2 [O] turn : ";
-    }
-    //Taking input from user
-    //updating the board according to choice and reassigning the turn Start
-
-    cin>> choice;
-
-    //switch case to get which row and column will be update
-
-    switch(choice){
-        case 1: row=0; column=0; break;
-        case 2: row=0; column=1; break;
-        case 3: row=0; column=2; break;
-        case 4: row=1; column=0; break;
-        case 5: row=1; column=1; break;
-        case 6: row=1; column=2; break;
-        case 7: row=2; column=0; break;
-        case 8: row=2; column=1; break;
-        case 9: row=2; column=2; break;
-        default:
-            cout<<"Invalid Move";
-    }
-
-    if(turn == 'X' && board[row][column] != 'X' && board[row][column] != 'O'){
-        //updating the position for 'X' symbol if
-        //it is not already occupied
-        board[row][column] = 'X';
-        turn = 'O';
-    }else if(turn == 'O' && board[row][column] != 'X' && board[row][column] != 'O'){
-        //updating the position for 'O' symbol if
-        //it is not already occupied
-        board[row][column] = 'O';
+    void init_board() {
+        char num = '1';
+        for (int i = 0; i < 3; ++i)
+            for (int j = 0; j < 3; ++j)
+                board[i][j] = num++;
         turn = 'X';
-    }else {
-        //if input position already filled
-        cout<<"Box already filled!n Please choose another!!nn";
-        player_turn();
     }
-    /* Ends */
-    display_board();
-}
 
-//Function to get the game status e.g. GAME WON, GAME DRAW GAME IN CONTINUE MODE
-
-bool gameover(){
-    //checking the win for Simple Rows and Simple Column
-    for(int i=0; i<3; i++)
-    if(board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[0][i] == board[1][i] && board[0][i] == board[2][i])
-    return false;
-
-    //checking the win for both diagonal
-
-    if(board[0][0] == board[1][1] && board[0][0] == board[2][2] || board[0][2] == board[1][1] && board[0][2] == board[2][0])
-    return false;
-
-    //Checking the game is in continue mode or not
-    for(int i=0; i<3; i++)
-    for(int j=0; j<3; j++)
-    if(board[i][j] != 'X' && board[i][j] != 'O')
-    return true;
-
-    //Checking the if game already draw
-    draw = true;
-    return false;
-}
-
-//Program Main Method
-
-int main()
-{
-    cout<<"tttT I C K -- T A C -- T O E -- G A M Ettt";
-    cout<<"nttttFOR 2 PLAYERSnttt";
-    while(gameover()){
-        display_board();
-        player_turn();
-        gameover();
+    void display_board() {
+        system("cls"); // "clear" jika di Linux
+        cout << "\nTIC TAC TOE\nPlayer [X] vs Komputer [O]\n\n";
+        for (int i = 0; i < 3; ++i) {
+            cout << "     |     |     \n";
+            cout << "  " << board[i][0] << "  |  " << board[i][1] << "  |  " << board[i][2] << "\n";
+            if (i != 2) cout << "_____|_____|_____\n";
+        }
+        cout << "     |     |     \n\n";
     }
-    if(turn == 'X' && draw == false){
-        cout<<"nnCongratulations!Player with 'X' has won the game";
+
+    bool is_valid_move(int choice, int &row, int &col) {
+        if (choice < 1 || choice > 9) return false;
+        row = (choice - 1) / 3;
+        col = (choice - 1) % 3;
+        return board[row][col] != 'X' && board[row][col] != 'O';
     }
-    else if(turn == 'O' && draw == false){
-        cout<<"nnCongratulations!Player with 'O' has won the game";
+
+    bool check_win() {
+        for (int i = 0; i < 3; ++i)
+            if ((board[i][0] == board[i][1] && board[i][1] == board[i][2]) ||
+                (board[0][i] == board[1][i] && board[1][i] == board[2][i]))
+                return true;
+        if ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
+            (board[0][2] == board[1][1] && board[1][1] == board[2][0]))
+            return true;
+        return false;
     }
-    else
-    cout<<"nnGAME DRAW!!!nn";
+
+    bool is_draw() {
+        for (int i = 0; i < 3; ++i)
+            for (int j = 0; j < 3; ++j)
+                if (board[i][j] != 'X' && board[i][j] != 'O')
+                    return false;
+        return true;
+    }
+
+    void player_turn() {
+        int choice, row, col;
+        while (true) {
+            cout << "Player [X] Masukkan posisi (1-9): ";
+            cin >> choice;
+            if (is_valid_move(choice, row, col)) {
+                board[row][col] = 'X';
+                moveHistory.push(choice);
+                break;
+            } else {
+                cout << "Langkah tidak valid. Ulangi.\n";
+            }
+        }
+    }
+
+    void ai_turn() {
+        // Rule-based AI: ambil langkah pertama yang kosong
+        for (int i = 1; i <= 9; ++i) {
+            int row, col;
+            if (is_valid_move(i, row, col)) {
+                board[row][col] = 'O';
+                moveHistory.push(i);
+                break;
+            }
+        }
+    }
+
+public:
+    void play() {
+        init_board();
+        giliran.push("Player");
+        giliran.push("AI");
+
+        while (true) {
+            display_board();
+            string current = giliran.front();
+            giliran.pop();
+
+            if (current == "Player") {
+                player_turn();
+            } else {
+                cout << "Komputer berpikir...\n";
+                ai_turn();
+            }
+
+            if (check_win()) {
+                display_board();
+                cout << "Pemenang: " << current << endl;
+                break;
+            } else if (is_draw()) {
+                display_board();
+                cout << "Permainan Seri!\n";
+                break;
+            }
+
+            giliran.push(current); // Masukkan giliran kembali
+        }
+
+        // Tampilkan urutan langkah
+        cout << "\nHistory Langkah (Stack): ";
+        vector<int> history;
+        while (!moveHistory.empty()) {
+            history.push_back(moveHistory.top());
+            moveHistory.pop();
+        }
+        reverse(history.begin(), history.end());
+        for (int step : history) cout << step << " ";
+        cout << endl;
+    }
+};
+
+int main() {
+    TicTacToe game;
+    game.play();
+    return 0;
 }
